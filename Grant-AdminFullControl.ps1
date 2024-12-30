6c2ec3f0843e385a0226555b1dcfe36f51c23036
GitAceHub <gitacehub@oryfox.de>
2024-12-30 19:42:52 +0100

    admin-acl

---
+++ Grant-AdminFullControl.ps1
@@ -0,0 +1,11 @@
+$NewAcl = Get-Acl -Path "Path"
+# Set properties
+$identity = "BUILTIN\Administrators"
+$fileSystemRights = "FullControl"
+$type = "Allow"
+# Create new rule
+$fileSystemAccessRuleArgumentList = $identity, $fileSystemRights, $type
+$fileSystemAccessRule = New-Object -TypeName System.Security.AccessControl.FileSystemAccessRule -ArgumentList $fileSystemAccessRuleArgumentList
+# Apply new rule
+$NewAcl.SetAccessRule($fileSystemAccessRule)
+Set-Acl -Path "Path" -AclObject $NewAcl
